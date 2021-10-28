d19d402c46c5373f68a45b3be13c2e92246bff81
yangsubinn <cindy010670@gmail.com>
2021-10-28 16:30:56 +0900

    [Add] Pod install

---
+++ UXThinkBig-SearchBar/UXThinkBig-SearchBar/Source/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UXThinkBig-SearchBar
+//
+//  Created by 양수빈 on 2021/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
